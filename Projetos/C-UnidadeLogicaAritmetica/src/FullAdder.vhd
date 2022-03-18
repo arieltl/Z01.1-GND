d25f651f5f6bbf5e1cbbb3b1247726afc132318f
Rafael Katri <rkatri20033@gmail.com>
2022-03-18 08:45:24 -0300

    Fix #36

--- Projetos/C-UnidadeLogicaAritmetica/src/FullAdder.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/FullAdder.vhd
@@ -22,4 +22,7 @@ architecture rtl of FullAdder is
 begin
   -- Implementação vem aqui!
 
+  soma <= (not a and not b and c) or (a and not b and not c) or (not a and b and not c) or (a and b and c);
+  vaium <= (a and b and not c) or (a and not b and c) or (not a and b and c) or (a and b and c);
+
 end architecture;
