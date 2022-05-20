21ba2b40cb249c1e84fe2020bdae3b59e8b0cd9e
Murilo Weyne <murilopw@al.insper.edu.br>
2022-05-20 09:48:38 -0300

    Fix #127

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -17,7 +17,52 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        if (!mnemnonic[0].equals("movw")) {
+            switch(mnemnonic[mnemnonic.length - 1]) {
+                case "%A" : return "0001";
+                case "%D" : return "0010";
+                case "(%A)" : return "0100";
+                default : return "0000";
+            }
+        }
+        if (mnemnonic.length == 3) {
+            switch(mnemnonic[2]) {
+                case "%A": return "0001";
+                case "%D": return "0010";
+                case "(%A)": return "0100";
+                default: return "0000";
+            }
+        }
+        if (mnemnonic.length == 4) {
+            switch (mnemnonic[2]+mnemnonic[3]) {
+
+                case "(%A)%D" :
+                case "%D(%A)" :
+                    return "0110";
+                case "(%A)%A" :
+                case "%A%A"   :
+                    return "0001";
+                case "%D%D"   : return "0010";
+                case "%D%A"   :
+                case "%A%D"   :
+                    return "0011";
+                default : return "0000";
+            }
+        }
+        if (mnemnonic.length == 5) {
+            switch (mnemnonic[2] + mnemnonic[3] + mnemnonic[4]) {
+
+                case "(%A)%A%D" :
+                case "%D(%A)%A" :
+                case "%D%A(%A)" :
+                case "%A%D(%A)" :
+                case "%A(%A)%D" :
+                case "(%A)%D%A" :
+                    return "0111";
+                default : return "0000";
+            }
+        }
+        return "0000";
     }
 
     /**
@@ -27,9 +72,131 @@ public class Code {
      */
     public static String comp(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
-    }
 
+        /* CASOS QUE RECEBEM UMA VARÍAVEL */
+
+        if (mnemnonic[0].startsWith("incw")) {
+            switch (mnemnonic[1]) {
+
+                case "%A"   : return "000110111";
+                case "%D"   : return "000011111";
+                case "(%A)" : return "001110111";
+                default     : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("notw")) {
+            switch (mnemnonic[1]) {
+                case "%A"   : return "000110001";
+                case "%D"   : return "000001101";
+                case "(%A)" : return "001110001";
+                default     : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("decw")) {
+            switch (mnemnonic[1]) {
+                case "%A"   : return "000110010";
+                case "%D"   : return "000001110";
+                case "(%A)" : return "001110010";
+                default     : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("negw")) {
+            switch (mnemnonic[1]) {
+                case "%A"   : return "000110011";
+                case "%D"   : return "000001111";
+                case "(%A)" : return "001110010";
+                default     : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("movw")) {
+            switch (mnemnonic[1]) {
+                case "$0"   : return "000101010";
+                case "$1"   : return "000111111";
+                case "$-1"  : return "000111010";
+                case "%A"   : return "000110000";
+                case "%D"   : return "000001100";
+                case "(%A)" : return "001110000";
+                default     : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("addw")) {
+            switch (mnemnonic[1] + mnemnonic[2]) {
+                case "$-1%A"  :
+                case "%A$-1"  :
+                    return "000110010";
+                case "$1%A"   :
+                case "%A$1"   :
+                    return "000110111";
+                case "$1%D"   :
+                case "%D$1"   :
+                    return "000011111";
+                case "(%A)$1" :
+                case "$1(%A)" :
+                    return "001110111";
+                case "%A%D" :
+                case "%D%A" :
+                    return "000000010";
+                case "(%A)%D" :
+                case "%D(%A)" :
+                    return "001000010";
+                default       : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("subw")) {
+            switch (mnemnonic[1] + mnemnonic[2]) {
+                case "%A$1"   : return "000110010";
+                case "%D$1"   : return "000001110";
+                case "(%A)$1" : return "001110010";
+                case "%A%D"   : return "000000111";
+                case "%D%A"   : return "000010011";
+                case "(%A)%D" : return "001000111";
+                case "%D(%A)" : return "001010011";
+                default       : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("rsubw")) {
+            switch (mnemnonic[1] + mnemnonic[2]) {
+                case "%A%D"   : return "000010011";
+                case "%D%A"   : return "000000111";
+                case "(%A)%D" : return "001010011";
+                case "%D(%A)" : return "001000111";
+                default       : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("andw")) {
+            switch (mnemnonic[1] + mnemnonic[2]) {
+                case "%A%D" :
+                case "%D%A" :
+                    return "000000000";
+                case "(%A)%D" :
+                case "%D(%A)" :
+                    return "001000000";
+                default : return "000000000";
+            }
+        }
+        if (mnemnonic[0].startsWith("orw")) {
+            switch (mnemnonic[1] + mnemnonic [2]) {
+                case "%A%D" :
+                case "%D%A" :
+                    return "000010101";
+                case "(%A)%D" :
+                case "%D(%A)" :
+                    return "001010101";
+                default : return "000000000";
+            }
+        }
+        if (    mnemnonic[0].startsWith("jmp")
+                || mnemnonic[0].startsWith("je")
+                || mnemnonic[0].startsWith("jne")
+                || mnemnonic[0].startsWith("jg")
+                || mnemnonic[0].startsWith("jge")
+                || mnemnonic[0].startsWith("jl")
+                || mnemnonic[0].startsWith("jle")) {
+            return "000001100";
+        } else {
+            return "000000000";
+        }
+    }
     /**
      * Retorna o código binário do mnemônico para realizar uma operação de jump (salto).
      * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
@@ -37,7 +204,18 @@ public class Code {
      */
     public static String jump(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]) {
+
+            case "jg" : return "001";
+            case "je" : return "010";
+            case "jge" : return "011";
+            case "jl"  : return "100";
+            case "jne" : return "101";
+            case "jle" : return "110";
+            case "jmp" : return "111";
+            default    : return "000";
+
+        }
     }
 
     /**
@@ -47,7 +225,14 @@ public class Code {
      */
     public static String toBinary(String symbol) {
         /* TODO: implementar */
-    	return "";
+        int i = Integer.parseInt(symbol);
+        String bin = Integer.toBinaryString(i);
+        String repeat = "";
+        if (bin.length() < 16){
+            String empty = "0";
+            repeat += empty.repeat(16-bin.length());
+        }
+        return repeat + bin;
     }
 
 }
